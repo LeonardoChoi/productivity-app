59584ffefa830b514dbff4a3c8cc3ecb84f8e7d3
unknown <leonardohchoi@gmail.com>
2023-05-31 14:02:33 -0300

    work on dailyGoal component

--- src/component/DailyGoal.js
+++ src/component/DailyGoal.js
@@ -1,5 +1,41 @@
+import React, { useState } from "react";
+
 const DailyGoal = () => {
-  return <div>Hello</div>;
+  const [dailyGoal, setDailyGoal] = useState("");
+
+  const handleChange = (event) => {
+    setDailyGoal(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    createDailyGoal(dailyGoal);
+    setDailyGoal("");
+  };
+
+  const createDailyGoal = (dailyGoal) => {
+    const updatedGoal = [
+      ...dailyGoal,
+      {
+        id: Math.floor(Math.random() * 9999),
+        name: dailyGoal,
+      },
+    ];
+    setDailyGoal(updatedGoal);
+  };
+
+  return (
+    <div>
+      <div className="createDailyGoal">
+        <h3>Create a New Daily Goal!</h3>
+        <form onSubmit={handleSubmit}>
+          <label>Daily Goal</label>
+          <input className="input" onChange={handleChange} value={dailyGoal} />
+          <button className="button">Create!</button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default DailyGoal;
