f1d2a0273e0c1a6dd6c16faf2302abad2337747c
unknown <leonardohchoi@gmail.com>
2023-06-02 20:08:21 -0300

    improve showGoal component

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ const App = () => {
   return (
     <div>
       <WeeklyGoal onCreate={createWeeklyGoal} />
-      <GoalsList dailyGoals={weeklyGoal} />
+      <GoalsList weeklyGoal={weeklyGoal} />
     </div>
   );
 };
--- src/component/ShowGoal.js
+++ src/component/ShowGoal.js
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
 function ShowGoal({ goal }) {
+  const [accomplishedHours, setAccomplishedHours] = useState("");
+  const [totalAccHours, setTotalAccHours] = useState(0);
+
+  // const changeHours = (e) => {
+  //   // const updatedHours = accomplishedHours + e.target.value;
+  // };
+  const totalAccHoursFunction = (hours) => {
+    const updatedHours = parseFloat(totalAccHours) + parseFloat(hours);
+    setTotalAccHours(updatedHours);
+  };
+
+  const handleChange = (e) => {
+    setAccomplishedHours(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    totalAccHoursFunction(accomplishedHours);
+    setAccomplishedHours("");
+  };
   return (
     <div>
-      <h1>{goal.goalTitle}</h1>
-      <p>{goal.goalHours}</p>
+      <h1>Goal: {goal.goalTitle}</h1>
+      <h3>
+        {totalAccHours}/{goal.goalHours} Hours |{" "}
+        {((totalAccHours / goal.goalHours) * 100).toFixed(2)}%
+      </h3>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="number"
+          value={accomplishedHours}
+          onChange={handleChange}
+        />
+        <button>Add </button>
+      </form>
     </div>
   );
 }
